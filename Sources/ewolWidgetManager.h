14f3adcaddeceadc956349014907975c2c6f6c36
Edouard Dupin <yui.heero@gmail.com>
2011-11-21 07:34:05 +0100

    oublie de fichier

---
+++ Sources/ewolWidgetManager.h
@@ -0,0 +1,39 @@
+/**
+ *******************************************************************************
+ * @file ewolWidgetManager.h
+ * @brief basic ewol Widget Manager (Header)
+ * @author Edouard DUPIN
+ * @date 13/11/2011
+ * @par Project
+ * ewol
+ *
+ * @par Copyright
+ * Copyright 2011 Edouard DUPIN, all right reserved
+ *
+ * This software is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY.
+ *
+ * Licence summary :
+ *    You can modify and redistribute the sources code and binaries.
+ *    You can send me the bug-fix
+ *
+ * Term of the licence in in the file licence.txt.
+ *
+ *******************************************************************************
+ */
+
+#ifndef __EWOL_WIDGET_MANAGER_H__
+#define __EWOL_WIDGET_MANAGER_H__
+
+#include <etkTypes.h>
+#include <ewolDebug.h>
+#include <ewolOObject.h>
+#include <etkVectorType.h>
+#include <etkSingleton.h>
+
+namespace ewol {
+	class widgetManager: public etk::Singleton
+	{
+		friend 
+	};
+};
