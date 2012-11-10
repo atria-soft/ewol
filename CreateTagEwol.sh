fa25024b32ba8cc054685cbdee821ba40f92391d
Edouard DUPIN <yui.heero@gmail.com>
2012-11-10 14:43:01 +0100

    [TREE] try to rework the tree of the librairie

--- CreateTagEwol.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-echo $* > Sources/libewol/tag
-git tag $*
--- Sources/libetk/etk/DebugInternal.cpp
+++
@@ -1,9 +0,0 @@
-/**
- * @author Edouard DUPIN
- * 
- * @copyright 2011, Edouard DUPIN, all right reserved
- * 
- * @license BSD v3 (see license file)
- */
-
-const char * etkLibName = "Etk      ";
