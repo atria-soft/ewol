f9ec8d0f12751cdb3a6138e85d856ba7eeadca02
Edouard DUPIN <yui.heero@gmail.com>
2013-12-28 16:08:13 +0100

    [DOC] add monk website

--- sources/monk_ewol.py
+++ sources/monk_ewol.py
@@ -10,6 +10,7 @@ def create():
 	myModule = monkModule.Module(__file__, 'ewol', 'LIBRARY')
 	# enable doculentation :
 	myModule.set_website("http://heeroyui.github.io/ewol/")
+	myModule.set_website_sources("http://github.com/heeroyui/ewol/")
 	myModule.set_path(tools.get_current_path(__file__) + "/ewol/")
 	myModule.set_path_general_doc(tools.get_current_path(__file__) + "/../doc/")
 	# add the currrent module at the 
